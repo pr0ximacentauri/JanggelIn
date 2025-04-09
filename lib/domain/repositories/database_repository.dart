596f4f3e3713ef46c068802ae43f6a15cf88cc52
pr0ximacentauri <rangga.pramudya23@gmail.com>
2025-04-09 14:16:06 +0700

    mengganti clean architecture ke MVVM

--- lib/domain/repositories/database_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:c3_ppl_agro/domain/models/device_status.dart';
-import 'package:c3_ppl_agro/domain/models/sensor.dart';
-
-abstract class DatabaseRepository {
-  Future<Sensor?> fetchLatestSensorData();
-  Future<DeviceStatus?> getDeviceStatus();
-  Future<void> updateDeviceStatus(bool isOn);
-}
---
+++ lib/models/services/supabase_service.dart
@@ -0,0 +1,18 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class SupabaseService {
+  static final SupabaseService _instance = SupabaseService._internal();
+  factory SupabaseService() => _instance;
+  
+  SupabaseService._internal();
+
+  static Future<void> init() async {
+    await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL'] ?? '',
+      anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+    );
+  }
+
+  static SupabaseClient get client => Supabase.instance.client;
+}
